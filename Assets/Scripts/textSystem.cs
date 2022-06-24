873f6b300ea33f20392f5c96d9eff11c37ef27ee
alfonsoquartny <seyrelmeyen2@outlook.com>
2022-06-24 15:28:48 +0300

    language system

--- Assets/Scripts/textSystem.cs
+++ Assets/Scripts/textSystem.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class textSystem : MonoBehaviour
 {
 
-    public string[] questionText;
     public string[] leftStrings;
     public string[] rightStrings;
+
+
+    public bool tr;
+    public bool eng;
+    public bool jp;
+
     public string[] nicks;
     public string[] infos;
+
     public Text question;
+
     private string Wording;
     private int quesIndex;
 
@@ -22,26 +30,68 @@ public class textSystem : MonoBehaviour
 
     public SpriteRenderer sr;
     public Sprite[] sprites;
+
+    public TextAsset theText;
+  public  string[] linesTR;
+    public string[] linesEn;
+    public string[] linesJp;
+    public Text subtitles;
+
+
+
+
     void Start()
     {
-        quesIndex = Random.Range(0, questionText.Length);
-       Wording = questionText[quesIndex];
+        linesTR = System.IO.File.ReadAllLines(@"Assets/languageTRdecision.txt");
+        linesEn = System.IO.File.ReadAllLines(@"Assets/languageENdecision.txt");
+         linesJp = System.IO.File.ReadAllLines(@"Assets/languageJPdecision.txt");
 
-       
+
+
+        quesIndex = Random.Range(0, linesEn.Length);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (tr == true)
+        {
             leftText.text = leftStrings[quesIndex];
             RightText.text = rightStrings[quesIndex];
             sr.sprite = sprites[quesIndex];
             nick.text = nicks[quesIndex];
             info.text = infos[quesIndex];
-   
 
-        question.text =""+Wording;
+            question.text = linesTR[quesIndex];
+        }
+
+        if (eng == true)
+        {
+            leftText.text = leftStrings[quesIndex];
+            RightText.text = rightStrings[quesIndex];
+            sr.sprite = sprites[quesIndex];
+            nick.text = nicks[quesIndex];
+            info.text = infos[quesIndex];
+
+            question.text = linesEn[quesIndex];
+        }
+
+        if (jp == true)
+        {
+            leftText.text = leftStrings[quesIndex];
+            RightText.text = rightStrings[quesIndex];
+            sr.sprite = sprites[quesIndex];
+            nick.text = nicks[quesIndex];
+            info.text = infos[quesIndex];
+
+            question.text = linesJp[quesIndex];
+        }
+          
+       
+
+
     }
 
 
